ae188303084f75bc9c4945d36ecec7609e3880d0
awantetodo <ignaciocasco@hotmail.com>
2022-06-16 16:47:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  /* ----------------------------- Spread operator ---------------------------- */
+  const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  // Declaro nombe en objeto
+   const objeto = {
+    id: 1,
+    nombre: "Ignacio"
+  }
+  const { nombre } = objeto;
+
+  // Destructuring con array 
+  const array2 = [
+    {
+      id: 1,
+      title: "Casco"
+    },
+    {
+      id: 2,
+      title: "Ignacio"
+    }
+  ]
+  const { title } = array2[1];
+  console.log(title)
+  const contenedorStyle = {
+    backgroundColor: "#ccc"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={contenedorStyle} className="App">
+      <h1>Primer entrega React.JS</h1>
+      <p>Accedo a la propiedad Title del objecto en el array con posicion 1 ** {title} **</p>
+      <p>Accedo a la propiedad Nombre del objeto ** {nombre} ** </p>
+      <h2>Imprimo mi array</h2>
+      {array2.map((persona) => (
+        <p>{persona.title}</p>
+      ))}
     </div>
   );
 }
